e5250b6d9d086c46665e0d633e4fa71d4af908e1
leon <silenceace@gmail.com>
2022-03-01 14:03:54 +0800

    feat: xcworkspace

---
+++ selftrack/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  selftrack
+//
+//  Created by leon yang on 2022/3/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ selftrack/selftrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  selftrackApp.swift
+//  selftrack
+//
+//  Created by leon yang on 2022/3/1.
+//
+
+import SwiftUI
+
+@main
+struct selftrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
